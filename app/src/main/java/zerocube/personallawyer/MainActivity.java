1aab8f8c1233e1b204b853752f6c5676c1a67c63
Debrockz <debrockz36@gmail.com>
2018-03-22 17:47:56 +0530

    Second commiting

--- app/src/main/java/zerocube/personallawyer/MainActivity.java
+++ app/src/main/java/zerocube/personallawyer/MainActivity.java
@@ -2,6 +2,7 @@ package zerocube.personallawyer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		//Toast here...
+        int b=3;
+        Toast.makeText(this, "B:"+b, Toast.LENGTH_SHORT).show();
     }
 }
